f63932a7e8dd1ed1af54a4e6c3024d733763e6a8
MitulThakral <mitul25csu147@ncuindia.edu>
2026-01-21 13:34:04 +0530

    prog9

---
+++ prog9.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter number";
+    cin>>n;
+    
+    
+    
+        if(n%3==0&&n%5==0)
+        {
+        cout<<" both "<<endl;
+        }
+        else if(n%5==0)
+        {
+        cout<<" buzz "<<endl;
+        }
+        else if(n%3==0)
+        {
+            cout<<"fuzz"<<endl;
+        }
+    
+    return 0;
+}
