e23856a21de90be7dd4bd81b4fcbdf74e9eefb86
MitulThakral <mitul25csu147@ncuindia.edu>
2026-01-29 22:16:46 +0530

    prog32

---
+++ prog32.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    int a[3][3],b[3][3],sum[3][3];
+    cout<<"enter values for 1st matrix";
+    for(i=0;i<3;i++)
+    {
+            for(j=0;j<3;j++)
+            {
+            cin>>a[i][j];
+            }
+    }
+        cout<<"enter values for 2nd matrix";
+        for(i=0;i<3;i++)
+{
+        for(j=0;j<3;j++)
+        {
+        cin>>b[i][j];
+        }
+}
+    for(i=0;i<3;i++)
+{
+        for(j=0;j<3;j++)
+{
+    sum[i][j]=a[i][j]+b[i][j];
+    cout<<sum[i][j];
+}
+cout<<endl;
+}
+return 0;
+}
