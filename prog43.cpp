770dde278debe03fab04559d7cf0cd0415dffef5
MitulThakral <mitul25csu147@ncuindia.edu>
2026-01-31 14:09:15 +0530

    prog43

---
+++ prog43.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,n;
+    cout<<"enter the number";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            char ch='A'+i-1;
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
