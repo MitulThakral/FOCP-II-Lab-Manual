dfd709b5edb2792323f355e5b40ed793742b1143
MitulThakral <mitul25csu147@ncuindia.edu>
2026-02-02 18:44:13 +0530

    prog52

---
+++ prog52.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int ans=0;
+    int arr[5];
+    cout<<"enter the numbers in array";
+    for(i=0;i<5;i++)
+    {
+        cin>>arr[i];
+    }
+    for(i=0;i<5;i++)
+    {
+        ans=ans^arr[i];
+    }
+    cout<<"unique elemnt in the array" <<ans<<endl;
+    return 0;
+}
