6a9b6d3b1be99689df352796c289cfc35066fc02
MitulThakral <mitul25csu147@ncuindia.edu>
2026-03-26 22:18:04 +0530

    Q8oops

---
+++ Q8oops.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+class Employee
+{
+    protected:
+    string name;
+    int salary;
+    public:
+    Employee(string name,int salary)
+    {
+        this->name=name;
+        this->salary=salary;
+    }
+};
+class Developer:public Employee
+{
+    public:
+    Developer(string name,int salary):Employee(name,salary){}
+    void displayrole()
+    {
+        cout<<"name of the developer is "<<name<<endl;
+        cout<<"salary of the developer is "<<salary<<endl;
+    }
+};
+class Designer:public Employee
+{
+    public:
+    Designer(string name,int salary):Employee(name,salary){}
+    void displayrole()
+    {
+        cout<<"name of the designer is "<<name<<endl;
+        cout<<"salary of thedesigner is "<<salary<<endl;
+    }
+};
+int main()
+{
+    Designer d1("rahul",50000);
+    Developer d2("mohit",40000);
+    d1.displayrole();
+    d2.displayrole();
+    return 0;
+}
