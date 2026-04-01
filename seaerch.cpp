19d85c30e8336bd71543d7d0113260c0fd7f2094
MitulThakral <mitul25csu147@ncuindia.edu>
2026-04-01 17:08:54 +0530

    linearsearch

---
+++ seaerch.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+bool search(int arr[],int size,int key)
+{
+    //base case
+    if(size==0)
+    {
+        return false;
+    }
+    if(arr[0]==key)
+    {
+        return true;
+    }
+    else{
+        bool remainingpart=search(arr+1,size-1,key);
+        return remainingpart;
+    }
+}
+int main()
+{
+    int arr[5]={3,5,1,2,6};
+    int size=5;
+    int key=8;
+    bool ans=search(arr,size,key);
+    cout<<ans;
+}
